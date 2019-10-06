e91ddda403d8fc8e77663dad5f50da202ab09438
Davide Serafini <davide@davideserafini.com>
2019-10-06 23:46:50 +0200

    Create frontend project and install dependencies

---
+++ frontend/webpack.config.dev.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.(sa|sc|c)ss$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+      } , {
+          loader: 'css-loader',
+        },
+        {
+          loader: 'postcss-loader'
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('sass')
+          }
+        }
+      ]
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'index.html'),
+    })
+  ],
+  entry: ['babel-polyfill', path.resolve(__dirname, 'src/js/index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  //devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    watchContentBase: true,
+    port: 9000,
+    publicPath: '/'
+  },
+  /* watch: true */
+};
---
+++ frontend/webpack.config.prod.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }, {
+      test: /\.(sa|sc|c)ss$/,
+      use: [{
+        loader: MiniCssExtractPlugin.loader,
+      } , {
+          loader: 'css-loader',
+        },
+        {
+          loader: 'postcss-loader'
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('sass')
+          }
+        }
+      ]
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, 'index.html'),
+    })
+  ],
+  entry: path.resolve(__dirname, 'src'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'production',
+};
